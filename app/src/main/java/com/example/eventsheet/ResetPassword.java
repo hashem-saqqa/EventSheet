c464c0a4f3781a421176ae2204e1a022b9fcb3c2
hashem <abobssam991@gmail.com>
2020-08-15 13:08:36 +0300

    Third screen done

--- app/src/main/java/com/example/eventsheet/ResetPassword.java
+++ app/src/main/java/com/example/eventsheet/ResetPassword.java
@@ -4,16 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.View;
+import android.widget.EditText;
 
 public class ResetPassword extends AppCompatActivity {
+    EditText edittext1;
+    EditText edittext2;
+    EditText edittext3;
+    EditText edittext4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
         getSupportActionBar().hide();
-
+        edittext1 = findViewById(R.id.InsertNumber1);
+        edittext2 = findViewById(R.id.InsertNumber2);
+        edittext3 = findViewById(R.id.InsertNumber3);
+        edittext4 = findViewById(R.id.InsertNumber4);
+        edittext1.setFilters(new InputFilter[]{new InputFilterMinMax("1","9")});
+        edittext2.setFilters(new InputFilter[]{new InputFilterMinMax("1","9")});
+        edittext3.setFilters(new InputFilter[]{new InputFilterMinMax("1","9")});
+        edittext4.setFilters(new InputFilter[]{new InputFilterMinMax("1","9")});
     }
     public void BACK(View view) {
         Intent myIntent = new Intent(getApplicationContext(), Login.class);
