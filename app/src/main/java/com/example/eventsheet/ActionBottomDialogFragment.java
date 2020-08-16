5a2874165356edcedcb074c26ef0efb70a13e944
hashem <abobssam991@gmail.com>
2020-08-16 19:27:35 +0300

    Third screen done

--- app/src/main/java/com/example/eventsheet/ActionBottomDialogFragment.java
+++ app/src/main/java/com/example/eventsheet/ActionBottomDialogFragment.java
@@ -3,28 +3,36 @@ package com.example.eventsheet;
 import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class ActionBottomDialogFragment extends BottomSheetDialogFragment {
+    LinearLayout linear1;
+    LinearLayout linear2;
+    LinearLayout linear3;
+
     public ActionBottomDialogFragment() {
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       View view = inflater.inflate(R.layout.bottom_sheet,container,false);
-        return view;
-
-    }
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.bottom_sheet, container, false);
+//        return view;
+//
+//    }
 
     @SuppressLint("RestrictedApi")
     @Override
@@ -32,6 +40,8 @@ public class ActionBottomDialogFragment extends BottomSheetDialogFragment {
         super.setupDialog(dialog, style);
         View view = View.inflate(getContext(), R.layout.bottom_sheet, null);
         dialog.setContentView(view);
-   }
 
+
+
+    }
 }
