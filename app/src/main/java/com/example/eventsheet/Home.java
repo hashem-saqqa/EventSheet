91d356de6a1e1414b0c8d821a48f1a8cf92ca8ab
hashem <abobssam991@gmail.com>
2020-09-14 21:55:37 +0300

    Third screen done

--- app/src/main/java/com/example/eventsheet/Home.java
+++ app/src/main/java/com/example/eventsheet/Home.java
@@ -1,19 +1,35 @@
 package com.example.eventsheet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class Home extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        toolbar = findViewById(R.id.MyToolBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
     public void BACK(View view) {
         Intent myIntent = new Intent(getApplicationContext(), Login.class);
         startActivityForResult(myIntent, 0);
