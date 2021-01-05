ba90159b87ff3797a510562b271368147d6055b2
hashem <abobssam991@gmail.com>
2021-01-05 23:44:45 +0200

    Home_screen_done

---
+++ app/src/main/java/com/example/eventsheet/Event_details.java
@@ -0,0 +1,17 @@
+package com.example.eventsheet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Event_details extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_details);
+    }
+
+}
--- app/src/main/java/com/example/eventsheet/Home.java
+++ app/src/main/java/com/example/eventsheet/Home.java
@@ -8,30 +8,26 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.material.appbar.AppBarLayout;
 
 public class Home extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         getSupportActionBar().hide();
-
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu,menu);
-        return true;
     }
 
     public void BACK(View view) {
         Intent myIntent = new Intent(getApplicationContext(), Login.class);
         startActivityForResult(myIntent, 0);
+    }
+
+    public void GoToEvent_details(View view) {
+        Intent intent = new Intent(getApplicationContext(), Event_details.class);
+        startActivityForResult(intent, 0);
     }
 }
