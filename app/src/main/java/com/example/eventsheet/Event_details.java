e92009bfe57744b5bef60e215a302cf3665ec2fd
hashem <abobssam991@gmail.com>
2021-01-08 03:32:31 +0200

    Home_screen_done

--- app/src/main/java/com/example/eventsheet/Event_details.java
+++ app/src/main/java/com/example/eventsheet/Event_details.java
@@ -6,12 +6,26 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class Event_details extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+        Fragment_1 fragment_1 = new Fragment_1();
+        getSupportFragmentManager().beginTransaction().add(R.id.Container,fragment_1).commit();
     }
 
+    public void add_fragment_1(View view) {
+        Fragment_1 fragment_1 = new Fragment_1();
+        getSupportFragmentManager().beginTransaction().add(R.id.Container,fragment_1).commit();
+    }
+
+    public void add_fragment_2(View view) {
+        Fragment_2 fragment_2 = new Fragment_2();
+        getSupportFragmentManager().beginTransaction().add(R.id.Container,fragment_2).commit();
+
+    }
 }
