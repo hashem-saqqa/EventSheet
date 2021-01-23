88ae0f5f0d3be0d553bf4593bc16ef7f22eeb111
hashem <abobssam991@gmail.com>
2021-01-24 00:40:15 +0200

    Home_screen_done

--- app/src/main/java/com/example/eventsheet/Event_details.java
+++ app/src/main/java/com/example/eventsheet/Event_details.java
@@ -1,31 +1,91 @@
 package com.example.eventsheet;
 
-import android.content.Intent;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
 
 public class Event_details extends AppCompatActivity {
+    private MapView mMapView;
+    public static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
         Fragment_1 fragment_1 = new Fragment_1();
         getSupportFragmentManager().beginTransaction().add(R.id.Container,fragment_1).commit();
+        mMapView = findViewById(R.id.user_list_map);
+
+        initGoogleMap(savedInstanceState);
     }
 
     public void add_fragment_1(View view) {
         Fragment_1 fragment_1 = new Fragment_1();
-        getSupportFragmentManager().beginTransaction().add(R.id.Container,fragment_1).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.Container,fragment_1).commit();
     }
 
     public void add_fragment_2(View view) {
         Fragment_2 fragment_2 = new Fragment_2();
-        getSupportFragmentManager().beginTransaction().add(R.id.Container,fragment_2).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.Container,fragment_2).commit();
 
     }
+    private void initGoogleMap(Bundle savedInstanceState){
+        // *** IMPORTANT ***
+        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+        // objects or sub-Bundles.
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+
+        mMapView.onCreate(mapViewBundle);
+try{
+    mMapView.getMapAsync((OnMapReadyCallback) this);
+
+}catch (Exception e){
+    Log.d("erorrrr", String.valueOf(e));
+    }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mMapView.onSaveInstanceState(mapViewBundle);
+    }
+//    @Override
+//    public void onMapReady(GoogleMap map) {
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED
+//                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        map.setMyLocationEnabled(true);
+//    }
 }
