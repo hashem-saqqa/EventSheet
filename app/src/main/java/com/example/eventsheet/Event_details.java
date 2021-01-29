cf8a755e7948864c03519662dd34c2f4740e8358
hashem <abobssam991@gmail.com>
2021-01-29 15:45:56 +0200

    Event_details_done

--- app/src/main/java/com/example/eventsheet/Event_details.java
+++ app/src/main/java/com/example/eventsheet/Event_details.java
@@ -1,23 +1,17 @@
 package com.example.eventsheet;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class Event_details extends AppCompatActivity {
-    private MapView mMapView;
-    public static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+public class Event_details extends AppCompatActivity implements OnMapReadyCallback {
+
 
 
     @Override
@@ -26,9 +20,10 @@ public class Event_details extends AppCompatActivity {
         setContentView(R.layout.activity_event_details);
         Fragment_1 fragment_1 = new Fragment_1();
         getSupportFragmentManager().beginTransaction().add(R.id.Container,fragment_1).commit();
-        mMapView = findViewById(R.id.user_list_map);
 
-        initGoogleMap(savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.MapContainer);
+        mapFragment.getMapAsync(this);
     }
 
     public void add_fragment_1(View view) {
@@ -41,51 +36,11 @@ public class Event_details extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.Container,fragment_2).commit();
 
     }
-    private void initGoogleMap(Bundle savedInstanceState){
-        // *** IMPORTANT ***
-        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
-        // objects or sub-Bundles.
-        Bundle mapViewBundle = null;
-        if (savedInstanceState != null) {
-            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
-        }
 
-        mMapView.onCreate(mapViewBundle);
-try{
-    mMapView.getMapAsync((OnMapReadyCallback) this);
-
-}catch (Exception e){
-    Log.d("erorrrr", String.valueOf(e));
-    }
-    }
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(21.483070, 39.184445)).title("jeddah"));
 
-        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
-        if (mapViewBundle == null) {
-            mapViewBundle = new Bundle();
-            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
-        }
-
-        mMapView.onSaveInstanceState(mapViewBundle);
     }
-//    @Override
-//    public void onMapReady(GoogleMap map) {
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-//                != PackageManager.PERMISSION_GRANTED
-//                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-//                != PackageManager.PERMISSION_GRANTED) {
-//            // TODO: Consider calling
-//            //    ActivityCompat#requestPermissions
-//            // here to request the missing permissions, and then overriding
-//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//            //                                          int[] grantResults)
-//            // to handle the case where the user grants the permission. See the documentation
-//            // for ActivityCompat#requestPermissions for more details.
-//            return;
-//        }
-//        map.setMyLocationEnabled(true);
-//    }
 }
