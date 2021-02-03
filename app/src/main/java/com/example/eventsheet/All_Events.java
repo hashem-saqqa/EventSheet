595312ba1d2da9cc2fb8c5bc3a9837b293c68b28
hashem <abobssam991@gmail.com>
2021-02-03 19:31:15 +0200

    Event_details_done

---
+++ app/src/main/java/com/example/eventsheet/All_Events.java
@@ -0,0 +1,14 @@
+package com.example.eventsheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class All_Events extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all__events);
+    }
+}
--- app/src/main/java/com/example/eventsheet/Event_details.java
+++ app/src/main/java/com/example/eventsheet/Event_details.java
@@ -10,25 +10,28 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Event_details extends AppCompatActivity implements OnMapReadyCallback {
-
+    View line;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
         Fragment_1 fragment_1 = new Fragment_1();
         getSupportFragmentManager().beginTransaction().add(R.id.Container,fragment_1).commit();
-
+        line = findViewById(R.id.horizontal_line2);
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.MapContainer);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
 
     public void add_fragment_1(View view) {
+        line.setBackgroundResource(R.drawable.multiple_colors1);
         Fragment_1 fragment_1 = new Fragment_1();
         getSupportFragmentManager().beginTransaction().replace(R.id.Container,fragment_1).commit();
     }
 
     public void add_fragment_2(View view) {
+        line.setBackgroundResource(R.drawable.multiple_colors2);
         Fragment_2 fragment_2 = new Fragment_2();
         getSupportFragmentManager().beginTransaction().replace(R.id.Container,fragment_2).commit();
 
@@ -38,6 +41,5 @@ public class Event_details extends AppCompatActivity implements OnMapReadyCallba
     @Override
     public void onMapReady(GoogleMap map) {
         map.addMarker(new MarkerOptions().position(new LatLng(21.483070, 39.184445)).title("jeddah"));
-
     }
 }
