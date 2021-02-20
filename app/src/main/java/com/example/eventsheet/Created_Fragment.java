155375f0f1f2a5766653062c33b5609b9dc2b09b
hashem <abobssam991@gmail.com>
2021-02-20 23:16:16 +0200

    All_Events

--- app/src/main/java/com/example/eventsheet/Created_Fragment.java
+++ app/src/main/java/com/example/eventsheet/Created_Fragment.java
@@ -1,8 +1,13 @@
 package com.example.eventsheet;
 
+import android.graphics.Canvas;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,6 +18,8 @@ import android.view.ViewGroup;
 import java.util.ArrayList;
 import java.util.List;
 
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
+
 public class Created_Fragment extends Fragment {
     protected List<Event_model> mDataset_my_created;
     protected RecyclerView mRecyclerView;
@@ -22,8 +29,14 @@ public class Created_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Create_events();
+
         return inflater.inflate(R.layout.fragment_created_, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Create_events();
     }
 
     public void Create_events() {
@@ -34,12 +47,44 @@ public class Created_Fragment extends Fragment {
                 "5/ 11/2021", "6/11/2021", "قيد المراجعة"));
         mDataset_my_created.add(new Event_model(R.drawable.nopath___copy__79_, "third event", "ksa",
                 "5/ 11/2021", "6/11/2021", "قيد المراجعة"));
+        mDataset_my_created.add(new Event_model(R.drawable.nopath___copy__79_, "fourth event", "ksa",
+                "5/ 11/2021", "6/11/2021", "قيد المراجعة"));
 
-        mRecyclerView = mRecyclerView.findViewById(R.id.recyclerView_created);
+        mRecyclerView = getView().findViewById(R.id.recyclerView_created);
         mLayoutManager = new LinearLayoutManager(getContext());
         mRecyclerView.setLayoutManager(mLayoutManager);
         my_created_events_adapter = new My_created_events_adapter(mDataset_my_created);
+        new ItemTouchHelper(itemSimpleCallback).attachToRecyclerView(mRecyclerView);
         mRecyclerView.setAdapter(my_created_events_adapter);
+
+
     }
+
+    ItemTouchHelper.SimpleCallback itemSimpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            mDataset_my_created.remove(viewHolder.getAdapterPosition());
+            my_created_events_adapter.notifyDataSetChanged();
+
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+            new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addSwipeLeftBackgroundColor(ContextCompat.getColor(getContext(),R.color.color_icon))
+                    .addSwipeLeftActionIcon(R.drawable.swipe_delete_icon)
+                    .addSwipeLeftLabel("حذف")
+                    .create()
+                    .decorate();
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    };
+
 
 }
