556a4d89e9bc61d0d07604b6fade9b3760e12e1c
hashem <abobssam991@gmail.com>
2021-03-03 22:56:37 +0200

    search filter is under the edit

---
+++ app/src/main/java/com/example/eventsheet/Search_filter.java
@@ -0,0 +1,17 @@
+package com.example.eventsheet;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+
+public class Search_filter extends Dialog {
+
+
+    public Search_filter(@NonNull Context context) {
+        super(context);
+        setContentView(R.layout.activity_search_filter);
+    }
+}
