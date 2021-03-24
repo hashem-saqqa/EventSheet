2c070432302a8426e6f19043f47a3c52dbcca959
hashem <abobssam991@gmail.com>
2021-03-24 23:39:28 +0200

    search filter is under the edit

--- app/src/main/java/com/example/eventsheet/Search_filter.java
+++ app/src/main/java/com/example/eventsheet/Search_filter.java
@@ -2,51 +2,72 @@ package com.example.eventsheet;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Objects;
 
 public class Search_filter extends BottomSheetDialogFragment {
+    final Calendar myCalendar = Calendar.getInstance();
 
+    EditText from_date;
+    EditText to_date;
 
     public Search_filter() {
-
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
     }
 
     @SuppressLint("RestrictedApi")
     @Override
     public void setupDialog(@NonNull Dialog dialog, int style) {
         super.setupDialog(dialog, style);
-        View view = View.inflate(getContext(),R.layout.activity_search_filter,null);
+        View view = View.inflate(getContext(), R.layout.activity_search_filter, null);
         dialog.setContentView(view);
 
-//        String[] years = {"1996","1997","1998","1998"};
+        from_date = view.findViewById(R.id.from_date);
+        to_date = view.findViewById(R.id.to_date);
 
-//        Spinner country_spinner = (Spinner) view.findViewById(R.id.country_spinner);
-//        ArrayAdapter<CharSequence> langAdapter = new ArrayAdapter<CharSequence>(getActivity(),
-//                R.layout.spinner_item, years );
-//        langAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        country_spinner.setAdapter(langAdapter);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+        from_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(getContext(), date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
 
-
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        from_date.setText(sdf.format(myCalendar.getTime()));
     }
 }
